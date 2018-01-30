054849069717fcd5c006e0eb574f3672af94498d
Sooyeong-Jo <sooyeong4988@naver.com>
2018-01-30 15:05:02 +0900

    로그인시 재료 보관일 update 관련 추가
    -

--- finalopen/src/main/resources/com/project/mybatis/RefriMapper.java
+++ finalopen/src/main/resources/com/project/mybatis/RefriMapper.java
@@ -1,6 +1,9 @@
 package com.project.mybatis;
 
 import java.util.HashMap;
+import java.util.List;
+
+import com.project.refri.Refri;
 
 public interface RefriMapper {
 	
@@ -8,5 +11,6 @@ public interface RefriMapper {
 	public void create_seq(String id);
 	public String selectR_num(String id, String num);
 	public void insertIngre(HashMap<String ,Object> map);
-
+	public List<Refri> selectAll(String refri_num);
+	public void updateTime(HashMap<String, Object> map);
 }
